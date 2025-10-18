cba39e52c44e3ce170b62f82721e3cc5bfb4afac
sumairahkanwal13 <sumairahkanwal33@gmail.com>
2025-10-19 01:10:04 +0500

    Fix wishlist item undefined issue

--- src/pages/Wishlist.js
+++ src/pages/Wishlist.js
@@ -3,33 +3,37 @@ import useCart from "../context/CartContext";
 import { useState } from "react";
 import { toast } from "react-toastify";
 
-export default function Wishlist(){
-    const  { wishlist, removeFromWishlist } = useWishlist();
-    const { addToCart } = useCart();
+export default function Wishlist() {
+  const { wishlist, removeFromWishlist } = useWishlist();
+  const { addToCart } = useCart();
 
-    const [ selectedItem, setSelectedItem ] = useState(null);
-    const [ selectedSize, setSelectedSize ] = useState("");
+  const [selectedItem, setSelectedItem] = useState(null);
+  const [selectedSize, setSelectedSize] = useState("");
 
-    const handleMoveToCart = (item) => {
-      if(item.sizes && item.sizes.length > 0 && !selectedSize && selectedItem?._id === item._id){
-        toast.error("Please select a size first!")
-        return;
-      }
+  const handleMoveToCart = (item) => {
+   
+    if (item.sizes && item.sizes.length > 0 && !selectedSize && selectedItem?._id === item._id) {
+      toast.error("Please select a size first!");
+      return;
     }
 
-    const productToAdd = () => {
-      item.sizes && item.sizes.length > 0 ? {...item, selectedSize} : item
+    const productToAdd =
+      item.sizes && item.sizes.length > 0
+        ? { ...item, selectedSize }
+        : item;
 
-      addToCart(productToAdd)
-      removeFromWishlist(item._id)
-      setSelectedItem(null)
-      setSelectedSize("")
-    }
+    addToCart(productToAdd);
+    removeFromWishlist(item._id);
+    toast.success("Moved to cart!");
+    setSelectedItem(null);
+    setSelectedSize("");
+  };
 
-    if(wishlist.length === 0){
-        return <h2 className="container mt-5">Wishlist is empty.</h2>
-    }
-return (
+  if (wishlist.length === 0) {
+    return <h2 className="container mt-5">Wishlist is empty.</h2>;
+  }
+
+  return (
     <div className="container my-4">
       <h1>Your Wishlist</h1>
       {wishlist.map((item) => (
@@ -51,30 +55,33 @@ return (
                   <strong>${item.price}</strong>
                 </p>
 
-                { selectedItem?._id === item._id && item.sizes && item.sizes.length > 0 (
-                  <select 
-                  className="form-select mb-2"
-                  value={selectedSize}
-                  onClick={() => setSelectedSize(e.target.value)}
+                
+                {selectedItem?._id === item._id && item.sizes && item.sizes.length > 0 && (
+                  <select
+                    className="form-select mb-2"
+                    value={selectedSize}
+                    onChange={(e) => setSelectedSize(e.target.value)}
                   >
                     <option value="">Select Size...</option>
                     {item.sizes.map((size) => (
-                      <option key={size} value={size} >{size}</option>
-                    ) )}
+                      <option key={size} value={size}>
+                        {size}
+                      </option>
+                    ))}
                   </select>
-                ) }
+                )}
 
                 <button
                   className="btn btn-primary me-2"
                   onClick={() => {
-                    if(item.size && item.size.length > 0 ) {
-                      setSelectedItem(item)
-                    }else {
-                      handleMoveToCart(item)
+                    if (item.sizes && item.sizes.length > 0 && selectedItem?._id !== item._id) {
+                      setSelectedItem(item);
+                    } else {
+                      handleMoveToCart(item);
                     }
                   }}
                 >
-                 {selectedItem?._id === item._id ? "Confirm Move" : "Move to cart"}
+                  {selectedItem?._id === item._id ? "Confirm Move" : "Move to Cart"}
                 </button>
 
                 <button
@@ -90,5 +97,4 @@ return (
       ))}
     </div>
   );
-
 }

06ec7bdab67b0d7697cc46ab53cd76830a4849c7
sumairahkanwal13 <sumairahkanwal33@gmail.com>
2025-10-22 12:07:20 +0500

    chores: Fixed UI of Address button.

--- src/pages/Address.js
+++ src/pages/Address.js
@@ -13,7 +13,7 @@ export default function Address(){
         <h1 className="container my-4">Address Info</h1>
         <button
           className="btn btn-primary"
-          style={{width:  "100px"}}
+          style={{minWidth: "200px"}}
           onClick={() => navigate("/addressForm")}
         >
           Add New Address

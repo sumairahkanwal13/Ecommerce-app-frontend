5bfa2c650904eb23ccf64e96dfb30c95e1d8aa72
sumairahkanwal13 <sumairahkanwal33@gmail.com>
2025-10-23 16:02:28 +0500

    chores: Fixed the footer margin.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,8 @@
 
 export default function Footer(){
     return(
-        <footer className="text-center bg-light mt-4">
-            <p className="">&copy; Fashion 21. All rights reserved.</p>
+        <footer className="text-center bg-light py-4 fixed-bottom" style={{marginTop: "40px"}}>
+            <p className="mb-0">&copy; Fashion 21. All rights reserved.</p>
         </footer>
     )
 }
